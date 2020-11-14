94542c2f56f9ffdedea74784fd78c616086fa70c
Laktoze <juliuspipikas82@gmail.com>
2020-11-14 21:14:23 +0200

    db setup

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "admin",
-    DB: "Commonwealth",
+    HOST: "w1kr9ijlozl9l79i.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
+    USER: "v2d2l8xiifb1r0v3",
+    PASSWORD: "q2z7t2233oniwi7n",
+    DB: "we58s3dpo8niwfii",
+    PORT: "3306",
     dialect: "mysql",
     pool: { // DAR NEZINAU KA DARO
       max: 5,
