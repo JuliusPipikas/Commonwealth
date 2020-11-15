e2c89baf7ba87eb4df960a0ac523acd8f4d9247b
Laktoze <juliuspipikas82@gmail.com>
2020-11-15 02:10:25 +0200

    auth21

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -55,12 +55,12 @@ exports.signin = (req, res) => {
         });
       }
 
-      var token = jwt.sign({ id: user.id }, config.secret, {
+      var token = jwt.sign({ user_id: user.user_id }, config.secret, {
         expiresIn: 86400 // 24 hours
       });
 
       res.status(200).send({
-        id: user.id,
+        user_id: user.user_id,
         username: user.username,
         role: user.role,
         accessToken: token
