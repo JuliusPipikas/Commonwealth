4342036e78e641bc8973eea00053f8e6440038b3
Laktoze <juliuspipikas82@gmail.com>
2020-12-07 19:18:41 +0200

    connect frontend

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  //origin: "http://localhost:8081"
-  origin: "https://drumian-commonwealth.herokuapp.com/"
+  origin: "http://localhost:8081"
+  //origin: "https://drumian-commonwealth.herokuapp.com/"
 };
 
 app.use(cors(corsOptions));
