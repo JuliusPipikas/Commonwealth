ddbc0f8b6da69860497443063e708c5f0d4d5d54
Laktoze <juliuspipikas82@gmail.com>
2020-12-08 01:20:03 +0200

    fix

---
+++ commonwealth-frontend/src/http-common.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "http://localhost:8080/api",
+  //baseURL: "https://the-drumian-commonwealth.herokuapp.com/api",
+  headers: {
+    "Content-type": "application/json"
+  }
+});
