f345e1912fc226e3d862e1695bae70502be1fbaa
Laktoze <juliuspipikas82@gmail.com>
2020-12-08 02:14:59 +0200

    Add client folder back to git repo

--- commonwealth-frontend/src/http-common.js
+++ commonwealth-frontend/src/http-common.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://the-drumian-commonwealth.herokuapp.com/api",
   //baseURL: "https://the-drumian-commonwealth.herokuapp.com/api",
   headers: {
-    "Content-type": "application/json"
-  }
-});
+    "Content-type": "ap
